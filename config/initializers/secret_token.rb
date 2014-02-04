03c65145cf2d644dda39ccda30436129b78dbace
gowrinaidur <gowrinaidur@gmail.com>
2014-02-04 08:53:07 +0530

    Installed Devise,CanCan and Rolify gems into this app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hms::Application.config.secret_token = '124be4c37f9dac623015bfda450bb222ec0205950174804e8b7f78ed03095e9ceb9ffd537c546321c9eca0b9722eaef15552fb27b8bfce6c4b8d2fcc3f5c9267'
